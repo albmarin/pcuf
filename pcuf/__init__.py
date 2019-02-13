8158f207d479c8212bdf6efcf8ad28aa8a96fb3b
Alberto Marin <alberto@ajmar.in>
2019-02-13 10:40:51 -0500

    Bump version: 0.1.1 → 0.1.2

--- pcuf/__init__.py
+++ pcuf/__init__.py
@@ -4,6 +4,6 @@ from . import file, functions, time, warnings
 
 __author__ = """Alberto J. Marin"""
 __email__ = "alberto@ajmar.in"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = ["file", "functions", "time", "warnings"]
