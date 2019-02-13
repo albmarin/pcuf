9003a261e2aa3c1ad921fa6fd7840ad1400b0c6c
Alberto Marin <alberto@ajmar.in>
2019-02-13 10:39:46 -0500

    Bug Fixes
    * Fixed import error

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     include_package_data=True,
     keywords="pcuf",
     name="pcuf",
-    packages=find_packages(include=["pcuf"]),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
